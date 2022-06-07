7ad552b234b817e5f76a543dc3eb7e4f5f0b107d
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-07 21:29:16 +0530

    added basic testing for the model

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Pseudo Blog application made using Django
 1. Developing a backend using django-rest framework for a blog application.
 2. Added Django structure for blog project and added post app.
 3. Adding model for post : Author, title , body , createdAt , updatedAt. will be using default auth model for the user. This will  import the user from or auth into the model and will help in avoiding duplication.
+4. Writing testcase for checking our model
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
+from cgi import test
+import imp
+from multiprocessing import AuthenticationError
 from django.test import TestCase
-
+from .models import Post
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #create a user
+        testUser1=User.objects.create_user(username="testUser1",password='abc123#')
+        testUser1.save()
+
+        #create a blog post
+        testPost=Post.objects.create(author=testUser1,title="Blog title", body="dummy text")
+        testPost.save()
+    
+    def testBlogData(self):
+        post=Post.objects.get(id=1)
+        author=f'{post.author}'
+        title=f'{post.title}'
+        body=f'{post.body}'
+        self.assertEqual(author,'testUser1')
+        self.assertEqual(title,'Blog title')
+        self.assertEqual(body,"dummy text")
