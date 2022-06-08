ac1ce359b8bd1a032190f825c612fdeef3aa38e1
Swapnil-Trivedi <swapniltrivedi1456@gmail.com>
2022-06-08 20:00:04 +0530

    Added rest_framework

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Pseudo Blog application made using Django
 1. Developing a backend using django-rest framework for a blog application.
 2. Added Django structure for blog project and added post app.
 3. Adding model for post : Author, title , body , createdAt , updatedAt. will be using default auth model for the user. This will  import the user from or auth into the model and will help in avoiding duplication.
-4. Writing testcase for checking our model
+4. Added basic testcase for checking our model for posts.
+5. Added Django rest framework and added default permissions
--- posts/tests.py
+++ posts/tests.py
@@ -12,12 +12,15 @@ class BlogTest(TestCase):
         #create a user
         testUser1=User.objects.create_user(username="testUser1",password='abc123#')
         testUser1.save()
-
+        testUser2=User.objects.create_user(username="testUser2",password="abc123")
+        testUser2.save()
         #create a blog post
         testPost=Post.objects.create(author=testUser1,title="Blog title", body="dummy text")
         testPost.save()
+        testPost2=Post.objects.create(author=testUser2,title="dum dum",body="random")
+        testPost2.save()
     
-    def testBlogData(self):
+    def testUser1(self):
         post=Post.objects.get(id=1)
         author=f'{post.author}'
         title=f'{post.title}'
@@ -25,3 +28,12 @@ class BlogTest(TestCase):
         self.assertEqual(author,'testUser1')
         self.assertEqual(title,'Blog title')
         self.assertEqual(body,"dummy text")
+
+    def testUser2(self):
+        post1=Post.objects.get(id=2)
+        author1=f'{post1.author}'
+        title1=f'{post1.title}'
+        body1=f'{post1.body}'
+        self.assertEqual(author1,'testUser2')
+        self.assertEqual(title1,'dum dum')
+        self.assertEqual(body1,"random")
